178c90c654f21247112f1f7afc0e1f64df0a4c80
Roma senkevuch <RomaSenkevich@bk.ru>
2021-06-02 19:16:55 +0300

    Initial Commit

---
+++ Take2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Take2
+//
+//  Created by MacBook Pro on 2.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Take2/Take2App.swift
@@ -0,0 +1,17 @@
+//
+//  Take2App.swift
+//  Take2
+//
+//  Created by MacBook Pro on 2.06.21.
+//
+
+import SwiftUI
+
+@main
+struct Take2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
